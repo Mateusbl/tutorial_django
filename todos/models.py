5c4c1fd81a35174fa3a53de7de23e740647f2733
Mateus Barbosa Lopes <mateusbarbosalopes999@gmail.com>
2024-07-16 07:50:54 -0300

    first commit

---
+++ todos/models.py
@@ -0,0 +1,13 @@
+from django.db import models
+
+
+class Todo(models.Model):
+    title = models.CharField(
+        verbose_name="Título", max_length=100, null=False, blank=False
+    )
+    created_at = models.DateTimeField(auto_now_add=True)
+    deadline = models.DateField(verbose_name="data de entrega", null=False, blank=False)
+    finished_at = models.DateField(null=True)
+
+
+# Create your models here.
---
+++ todos/views.py
@@ -0,0 +1,14 @@
+from django.urls import reverse_lazy
+from django.views.generic import ListView, CreateView
+
+from .models import Todo
+
+
+class TodoListview(ListView):
+    model = Todo
+
+
+class TodoCreateView(CreateView):
+    model = Todo
+    fields = ["title", "deadline"]
+    success_url = reverse_lazy("todo_list")
